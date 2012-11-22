b1eb9511d76fc8c2d7869fd63170112e9f04467f
gavrilyak <gavrilyak@gmail.com>
2012-11-22 11:59:56 +0200

    mix options on call

--- Modal.js
+++ Modal.js
@@ -206,6 +206,8 @@ define([
                 var data = support.getData(node, 'modal');
                 if (!data) {
                     support.setData(node, 'modal', (data = new Modal(node, options)));
+                } else {
+                    lang.mixin(data.options, options);
                 }
                 if (lang.isString(option)) {
                     data[option].call(data);
